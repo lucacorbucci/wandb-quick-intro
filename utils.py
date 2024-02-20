6e0ab1db12651d77526582ef66afdb7ecd0fe658
lucacorbucci <corbuccilu@gmail.com>
2024-02-20 09:55:19 +0000

    Formatted file with ruff

--- utils.py
+++ utils.py
@@ -2,9 +2,13 @@ import requests
 import time
 from datetime import datetime
 import torch.nn.functional as F
-import wandb 
+import wandb
+
+
 def download_weather_data():
-    response = requests.get("https://api.open-meteo.com/v1/forecast?latitude=41.390205&longitude=2.154007&hourly=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation_probability&forecast_days=1")
+    response = requests.get(
+        "https://api.open-meteo.com/v1/forecast?latitude=41.390205&longitude=2.154007&hourly=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation_probability&forecast_days=1"
+    )
     if response.status_code == 200:
         print("sucessfully fetched the data")
         return response.json()
@@ -14,17 +18,28 @@ def download_weather_data():
 
 def weather_data_generator():
     weather_data = download_weather_data()
-    for measure in range(len(weather_data['hourly']['time'])):
-        hour = int(datetime.fromisoformat(weather_data['hourly']['time'][measure]).hour)
-        temperature = weather_data['hourly']['temperature_2m'][measure]
-        humidity = weather_data['hourly']['relative_humidity_2m'][measure]
-        apparent_temperature = weather_data['hourly']['apparent_temperature'][measure]
-        precipitation_probability = weather_data['hourly']['precipitation_probability'][measure]
+    for measure in range(len(weather_data["hourly"]["time"])):
+        hour = int(datetime.fromisoformat(weather_data["hourly"]["time"][measure]).hour)
+        temperature = weather_data["hourly"]["temperature_2m"][measure]
+        humidity = weather_data["hourly"]["relative_humidity_2m"][measure]
+        apparent_temperature = weather_data["hourly"]["apparent_temperature"][measure]
+        precipitation_probability = weather_data["hourly"]["precipitation_probability"][
+            measure
+        ]
         time.sleep(2)
-        yield hour, temperature, humidity, apparent_temperature, precipitation_probability
+        yield (
+            hour,
+            temperature,
+            humidity,
+            apparent_temperature,
+            precipitation_probability,
+        )
+
 
 # convenience funtion to log predictions for a batch of test images
-def log_test_predictions(images, labels, outputs, predicted, test_table, log_counter, NUM_IMAGES_PER_BATCH):
+def log_test_predictions(
+    images, labels, outputs, predicted, test_table, log_counter, NUM_IMAGES_PER_BATCH
+):
     # obtain confidence scores for all classes
     scores = F.softmax(outputs.data, dim=1)
     log_scores = scores.cpu().numpy()
